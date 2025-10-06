8fd92026fb3d52a98bd38577b8911c436441cd5e
Student <Student@itf-122-teacher.itf.local>
2025-10-06 18:35:35 +0500

    DAily commit

---
+++ src/lesson2/FilesTest.java
@@ -0,0 +1,37 @@
+package lesson2;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+public class FilesTest {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		final File	f = new File("c:/users/student/test.txt");
+		final Path	p = f.toPath();
+		
+		System.err.println("Exists(1): "+f.exists());
+		System.err.println("Exists(2): "+Files.exists(p));
+
+		System.err.println("IsDirectory(1): "+f.isDirectory());
+		System.err.println("IsDirectory(2): "+Files.isDirectory(p));
+	
+		System.err.println("Name(1):"+f.getName());
+		System.err.println("Name(2):"+p.getName(p.getNameCount()-1));
+
+		final File	f2 = new File("c:/users/student");
+		final Path	p2 = f2.toPath();
+		
+		System.err.println("List(1):"+Arrays.toString(f2.listFiles()));
+
+		try(DirectoryStream<Path> stream = Files.newDirectoryStream(p2)) {
+			for(Path item : stream) {
+				System.err.println("Item="+item);
+			}
+		}
+	}
+}
